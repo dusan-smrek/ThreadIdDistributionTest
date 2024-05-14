ad25b228685eb874ee76cd007572208e463e8cd1
Dušan Smrek <63956080+dusan-smrek@users.noreply.github.com>
2024-05-14 13:58:23 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,10 +6,11 @@ public class Program
 {
     public static void Main()
     {
-        Console.WriteLine("Hello Range World");
+        Console.WriteLine("Hello Thread Id statistics World");
         var maxAttempts = 15;
         IDictionary<int, int> results = new Dictionary<int, int>();
 
+        // create tasks and collect their respective ThreadId
         for (int i = 0; i < maxAttempts; i++)
         {
             var t = Task<int>.Run(() =>
@@ -23,6 +24,7 @@ public class Program
             results.Add(i + 1, t.Result);
         }
 
+        // list overview
         Console.WriteLine("Details:");
 
         foreach (var item in results)
@@ -30,6 +32,7 @@ public class Program
             Console.WriteLine("Attempt = {0}, Thread Id = {1}", item.Key, item.Value);
         }
 
+        // create new dictionary containing a statistical summary of count of Thread Ids assigned per run
         var query = 
             from q in results
             group q by q.Value into t
